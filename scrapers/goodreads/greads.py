c0855bd1222a8f3743534fc4942d891eda39c209
ppeczak0405@gmail.com <ppeczus0405>
2018-02-02 23:40:02 +0100

    Listowanie Ksiazek

--- scrapers/goodreads/greads.py
+++ scrapers/goodreads/greads.py
@@ -1,16 +1,22 @@
 from goodreads import client
 import json
 
-with open('../../config.json', 'r') as f:
-    dict = json.load(f)
-    f.close()
+def get_client():
+    with open('../../config.json', 'r') as f:
+        dict = json.load(f)
 
-client_id = dict['goodreads']['client_id']
-key = dict['goodreads']['key']
+    client_id = dict['goodreads']['client_id']
+    key = dict['goodreads']['key']
 
-gc = client.GoodreadsClient(client_id, key)
-gc.authenticate(client_id, key)
+    gc = client.GoodreadsClient(client_id, key)
+    gc.authenticate(client_id, key)
+
+    return gc
 
 def search_books(text_to_find):
+    gc = get_client()
     books = gc.search_books(text_to_find, 1, 'all')
     return books[:10]
+
+if __name__ == '__main__':
+    print(search_books("Psychology"))
