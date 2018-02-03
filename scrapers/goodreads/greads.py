2c6311b578aabdc3b1307edd018e7c71c609a6f1
ppeczak0405@gmail.com <ppeczus0405>
2018-02-03 06:52:32 +0100

    Listowanie uzytkownikow

--- scrapers/goodreads/greads.py
+++ scrapers/goodreads/greads.py
@@ -7,24 +7,19 @@ def get_client():
     with open(path, 'r') as f:
         dict = json.load(f)
 
-    client_id = dict['goodreads']['client_id']
-    key = dict['goodreads']['key']
+    client_id = dict['services']['goodreads']['client_id']
+    key = dict['services']['goodreads']['key']
 
     gc = client.GoodreadsClient(client_id, key)
     gc.authenticate(client_id, key)
 
     return gc
 
-def search_books(text_to_find, n):
-    gc = get_client()
-    books = gc.search_books(text_to_find, 1, 'all')
-    return books[:5]
+def search_books(category, n):
+    with open(path, 'r') as f:
+        dict = json.load(f)['services']['goodreads']
+    return dict[category][:n]
 
 if __name__ == '__main__':
     with open(path, 'r') as f:
-        dict = json.load(f)['goodreads']
-    print(search_books(dict['mathematics'], 5))
-    print(search_books(dict['cs'], 5))
-    print(search_books(dict['economics'], 5))
-    print(search_books(dict['psychology'], 5))
-    print(search_books(dict['sport'], 5))
+        dict = json.load(f)['services']['goodreads']
