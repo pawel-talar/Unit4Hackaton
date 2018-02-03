41ad21d141edc7b2cbb0fc9cfdea17ed970323e5
Łukasz Hryniuk <lukasz.hryniuk@wp.pl>
2018-02-03 09:26:18 +0100

    Add sql scripts

---
+++ sql.py
@@ -0,0 +1,48 @@
+import sqlite3
+import os.path
+import logging
+import time
+from os.path import expanduser
+import requests
+
+
+profile_name = 'mxhml4y1.default'
+history_path = expanduser("~") + '/.mozilla/firefox/' + profile_name + \
+               '/places.sqlite'
+
+
+def connect_to_sqlite(db_path):
+    conn = sqlite3.connect(db_path)
+    return conn
+
+
+already_visited = set()
+
+def get_urls(conn):
+    global already_visited
+    query_str = """
+SELECT p.url FROM moz_historyvisits AS h, moz_places AS p
+WHERE p.id == h.place_id
+ORDER BY h.visit_date"""
+
+    c = conn.cursor()
+    urls = set([row[0] for row in c.execute(query_str)])
+    to_return = urls - already_visited
+    already_visited = already_visited | to_return
+    return list(to_return)
+
+
+if __name__ == '__main__':
+    delay = 5
+    logging.basicConfig(level=logging.DEBUG)
+    print(history_path)
+    conn = connect_to_sqlite(history_path)
+    while True:
+        time.sleep(delay)
+        urls = [url for url in get_urls(conn) if 'wikipedia' in url]
+        for url in urls:
+            ru = 'http://localhost:5001/submit'
+            headers = {"Content-Type": "text/plain"}
+            logging.info("ru: {} data: {} headers: {}".format(ru, url, headers))
+            requests.post(ru, data=url, headers=headers)
+
